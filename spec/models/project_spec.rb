80b2e7dcb8a78248ba73b0c5173b4be688f9cb02
Harry Frankel <hfranks92@gmail.com>
2020-11-04 23:07:04 +1100

    Whitespace removed

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   subject { create(:project) }
-  describe 'Validations' do  
+  describe 'Validations' do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_length_of(:title).is_at_most(150) }
     it { is_expected.to validate_uniqueness_of(:title) }
