5ac5af046250ca05b40714cc55aa50b11f5e43a8
Harry Frankel <hfranks92@gmail.com>
2020-11-05 15:21:35 +1100

    Generate projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+  get 'projects/new'
+  get 'projects/create'
+  get 'projects/show'
+  get 'projects/edit'
+  get 'projects/update'
   root to: 'home#index'
   devise_for :users
 end
