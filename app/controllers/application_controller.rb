1e6e38197c0157f8d00cbdee39000af7f60918ac
Harry Frankel <hfranks92@gmail.com>
2020-11-23 18:05:43 +1100

    Create projects controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
+  before_action :configure_account_update_params, only: [:update], if: :devise_controller?
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:given_names, :last_name])
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:given_names, :last_name])
+  end
 end
--- spec/controllers/projects_spec.rb
+++ spec/controllers/projects_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
-  context 'Params' do
-    it do
-      is_expected.to permit(:title, :description, :user_id)
-        .for(:create)
-        .on(:project)
-    end
 
-    it { is_expected.to use_before_action(:project_params) }
-  end
 end
+
