776ea0a0dc64dbc603c9e1ef25af956206b1b6ad
Harry Frankel <hfranks92@gmail.com>
2020-11-23 18:31:46 +1100

    Revert "Convert to rails api mode"
    This reverts commit 0fdec9e13626cfddc690acfa73967169de946466.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
   before_action :configure_account_update_params, only: [:update], if: :devise_controller?
 
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::API
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[given_names last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:given_names, :last_name])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[given_names last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:given_names, :last_name])
   end
 end
