c7676bf9a8d3aa27907057b076fd6c0f5e9cbe58
Harry Frankel <hfranks92@gmail.com>
2020-11-23 22:08:05 +1100

    Create seed file and add faker

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 gem 'devise', '~> 4.7'
 gem 'react-rails'
+
+gem "faker", "~> 2.14", :group => :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+1..50.times do |_x|
+  user_params = {
+    password: Faker::Internet.password(min_length: 10, max_length: 20),
+    given_names: Faker::Name.name,
+    last_name: Faker::Name.last_name
+  }
+  user_params[:email] = Faker::Internet.email(name: user_params[:given_names]).to_s
+  user = User.new(user_params)
+  puts 'new user' if user.save!
+
+  project_params = {
+    title: Faker::Company.name,
+    description: Faker::Company.catch_phrase,
+    user_id: User.all.sample.id
+  }
+  project = Project.new(project_params)
+  puts 'new project' if project.save!
+
+  project_collab_params = {
+    user_id: User.all.sample.id,
+    project_id: Project.all.sample.id
+  }
+  project_collab = ProjectCollaborator.new(project_collab_params)
+  puts 'new collaborator' if project_collab.save!
+end
